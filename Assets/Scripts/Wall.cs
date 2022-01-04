7bce9adab515e99c4b43869eaba44463749e03d4
Daniel <gaa848@gmail.com>
2022-01-04 17:27:02 +0100

    Removed unnecessary files

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Car car = collision.transform.GetComponent<Car>();
+
+        if (car != null)
+        {
+            car.DisableCar();
+        }
+    }
+}
